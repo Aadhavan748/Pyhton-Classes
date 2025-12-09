2325ca780bcee0f6ff3bcdcecf2f4c8c606090fc
Aadhavan748 <appleid16400@gmail.com>
2025-12-09 17:32:30 +0800

    Add files via upload

---
+++ Class2.py
@@ -0,0 +1,30 @@
+"""Welcome to the world of coding
+Outline:
+Write a program for print command.
+print("Welcome to the world of programming!")
+print(4)
+print("Hello \nPython")
+print("Hello" , 5)
+print("Welcome to" , end= "*")"""
+
+
+
+"""Understanding the variables
+Outline:
+Write a program to create different variables to store various values.
+x = 5
+y = "John"
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal)
+name = input("Enter your name: ")
+print("Hello" , name , "Welcome to codingal!")"""
+
+
+
+"""Keywords
+Outline:
+Write a program to print all the keywords
+import keyword
+print(keyword.kwlist)"""
