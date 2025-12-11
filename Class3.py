538d11e6d42d80e90a50209effc7963027fb021c
Aadhavan748 <appleid16400@gmail.com>
2025-12-11 18:47:38 +0800

    Add files via upload

---
+++ Class3.py
@@ -0,0 +1,46 @@
+# Activity 3
+
+# input a word
+##text = (input("Enter a string: "))
+
+#word = "Computer"
+#print(word[::-1])
+
+"""revText = text[::-1]
+text1 = revText
+print(text1)"""
+
+# Activity 1
+
+"""a = 2
+print("type of a: ", type(a))
+b = 6.4
+print("type of b: ", type(b))
+c = "Aadhavan"
+print("type of c: ", type(c))
+d = False
+print("type of d: ", type(d))"""
+
+# Activity 2
+
+name = "John"
+age =  12
+student = True
+weight = 34.8
+
+print("Name: ", name)
+print("Datatype of Name is: ", type(name))
+print("Age: ", age)
+print("Datatype of age is: ", type(age))
+print("Student: ", student)
+print("Datatype of student is: ", type(student))
+print("Weight is: ", weight)
+print("Datatype of weight is: ", type(weight))
+
+print("\n After Type Casting:")
+age = str(age)
+print(age)
+print("Datatype of age is: ", type(age))
+weight = int(weight)
+print(weight)
+print("Datatype of weight is: ", type(weight))
