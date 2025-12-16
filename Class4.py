51b856a70db1c35da9265b4fbfc01929638deb09
Aadhavan748 <appleid16400@gmail.com>
2025-12-16 17:34:21 +0800

    Add files via upload

---
+++ Class4.py
@@ -0,0 +1,20 @@
+# Activity 1
+"""
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all the trees is: ", sum)
+average = sum/5 
+print("The average of all the trees is :", average)"""
+
+# Activity 2
+
+Amount = int(input("Please Enter Amount for Withdrawal: "))
+note100 = Amount//100
+note500 = Amount//500
+print(note100)
+print(note500)
