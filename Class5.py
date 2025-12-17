055058cfab4c0e88433fb5afd89972c1dc85e114
Aadhavan748 <appleid16400@gmail.com>
2025-12-17 18:28:12 +0800

    Add files via upload

---
+++ Class5.py
@@ -0,0 +1,36 @@
+# Activity 1
+
+"""num = int(input("Enter the number: "))
+if num > 0:
+    print(num, "is a positive number")
+else:
+    print(num, "is not a positive number")"""
+
+# Activity 2
+
+"""actualcost = float(input("Enter cost price: "))
+saleamount = float(input("Enter selling price: "))
+
+if(saleamount > actualcost):
+    amount = saleamount - actualcost
+    print("Total profit = {0}".format(amount))
+else:
+    print("No profit")"""
+
+# Activity 3
+
+"""i = int(input("Enter a number: "))
+if(i < 15):
+    print("i is smaller than 15")
+    print("I'm in if block")
+else:
+    print("i is greater than 15")
+    print("I'm in else block")"""
+
+# Activity 4
+
+number = int(input("Enter the number: "))
+if number%2==0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
