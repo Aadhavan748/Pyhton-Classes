da29b720897c32da8306e0dc11c00f632bc7a0de
Aadhavan748 <appleid16400@gmail.com>
2025-12-18 18:29:38 +0800

    Add files via upload

---
+++ Class6.py
@@ -0,0 +1,50 @@
+# Activity 3
+
+"""height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+
+BMI = weight / (height/100)**2
+
+print("Your BMI is", BMI)
+if BMI <= 18.4:
+    print("You are underweight")
+elif BMI <= 24.9:
+    print("You are healthy")
+elif BMI <= 29.9:
+    print("You are overweight")
+elif BMI <= 34.9:
+    print("You are obese")
+else:
+    print("You are severely obese")"""
+
+# Activity 1
+
+"""a = 13
+b = -3
+c = -7
+
+if a > 0 or b > 0:
+    print("Either one of the numbers are greater than 0")
+else:
+    print("No number is greater than 0")
+if b > 0 or c > 0:
+    print("Either one of the numbers are greater than 0")
+else:
+    print("No number is greater than 0")"""
+
+# Activity 2
+
+a = 0
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+a = "Pyhton"
+b = "Coding"
+
+if a != b:
+    print(a, 'and', b, "are different")
+else:
+    print(a, "and", b, "are both the same")
