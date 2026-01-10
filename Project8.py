fd7e6a0d0d26d0b943586644a46ac0ca7977287d
Aadhavan748 <appleid16400@gmail.com>
2026-01-10 19:59:36 +0800

    Add files via upload

---
+++ Project8.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+print("The first number is: ", c)
+print("The second number is: ", b)
+print("The third number is: ", a)
