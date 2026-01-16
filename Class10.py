9471934a219a3db91e36dfd58e160719b75f2af3
Aadhavan748 <appleid16400@gmail.com>
2026-01-16 20:13:10 +0800

    Add files via upload

---
+++ Class10.py
@@ -0,0 +1,39 @@
+"""for i in range(31):
+    print(i)
+
+for i in range(5,41):
+    print(i)
+
+for i in range(2,21,3):
+    print(i)
+
+name = "Laptop"
+for djisidgfisdiujf in name:
+    print(djisidgfisdiujf)
+
+# Activity 1 - Sum of whole numbers
+
+n = int(input("Enter the number to find the sum: "))
+sum = 0
+
+for i in range(1, n+1):
+    sum = sum+i
+    print("\nSum =", sum)
+
+# Activity 2 - Reverse a String
+
+string = input("Please enter your string: ")
+string2 = ("")
+
+for i in string:
+    string2 = i + string2
+
+print("\nThe original string = ", string)
+print("\nThe reversed string = ", string2)"""
+
+# Activity 3 - Reverse Order
+
+n = int(input("Enter the value of n: "))
+
+for i in range(n,0,-1):
+    print(i)
