150efbe2c17e2a40889d70ff577bc5dcb50be622
Aadhavan748 <appleid16400@gmail.com>
2026-01-16 20:24:41 +0800

    Add files via upload

---
+++ Project10.py
@@ -0,0 +1,4 @@
+num = int(input("Enter the number you want to power with: "))
+power = int(input("Enter the number you want to be the power: "))
+power_value = num**power
+print(num, "to the power of", power, "is",power_value)
