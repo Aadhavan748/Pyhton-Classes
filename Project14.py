d13e12db54f118504a6bb01714b09005adc15afa
Aadhavan748 <appleid16400@gmail.com>
2026-02-01 20:18:12 +0800

    Add files via upload

---
+++ Project14.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+
+turtle.done()
