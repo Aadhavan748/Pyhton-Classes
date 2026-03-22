9dc7dfc9904bf72ed0a0e90b2db728a396d2142e
Aadhavan748 <appleid16400@gmail.com>
2026-03-22 18:50:49 +0800

    Add files via upload

---
+++ Project24.py
@@ -0,0 +1,10 @@
+Set1 = {'blue', 'green'}
+Set2 = {'blue', 'yellow'}
+print("A:", Set1.symmetric_difference(Set2))
+
+Set1 = {1, 2, 3, 4, 5}
+Set2 = {1, 5, 6, 7, 8, 9}
+print("B:", Set1.symmetric_difference(Set2))
+
+A: {'green', 'yellow'}
+B: {2, 3, 4, 6, 7, 8, 9}
